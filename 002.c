3350b74a8e18a49af0a8d6273826ead3b4d0b4c9
Jamie <jbaggott15@gmail.com>
2014-04-12 22:17:45 +0100

    added 002.c

---
+++ 002.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int fib(x)
+{
+	if ((x == 1) || (x == 2))
+		return x;
+	else
+		return (fib(x-1) + fib(x-2));
+}
+
+int main()
+{
+	int x = 2, n = 2, sum = 0;
+	while (n <= 4000000) {
+		sum += n;
+		x += 3;
+		n = fib(x);
+	}
+	printf("%d\n", sum);
+	return 0;
+}
